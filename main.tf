ca8f6b88db3dcada99abf953180e54d2ef6a8114
ChandraSekhar-web <67157076+ChandraSekhar-web@users.noreply.github.com>
2024-12-18 10:56:01 +0530

    Update main.tf by Sekhar

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.9.6" // added latest version
+    required_version = "~> 1.10.2" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.74.0"
+      version = "~> 5.81.0"
      }
     }
    }
